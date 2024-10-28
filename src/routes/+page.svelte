36e91f306d76df07ed742cadb828be6ae5df5fae
Enqu1 <114190936+Enqu1@users.noreply.github.com>
2024-10-28 15:02:13 +0000

    art work + user selecting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import paper from "$lib/assets/paper.png";
+  import rock from "$lib/assets/rock.png";
+  import scissors from "$lib/assets/scissors.png";
   import { fly } from "svelte/transition";
   let playerChoice: string;
 
@@ -8,17 +11,43 @@
 </script>
 
 <div class="game">
-  <button on:click={() => makeChoice("rock")}>Rock</button>
-  <button on:click={() => makeChoice("paper")}>Paper</button>
-  <button on:click={() => makeChoice("scissors")}>Scissors</button>
+  <button
+    on:click={() => makeChoice("rock")}
+    class="btn-choice btn-rock selecting"
+  >
+    <img src={rock} alt="rock" /></button
+  >
+  <button
+    on:click={() => makeChoice("paper")}
+    class="btn-choice btn-paper selecting"
+    ><img src={paper} alt="paper" /></button
+  >
+  <button
+    on:click={() => makeChoice("scissors")}
+    class="btn-choice btn-scissors selecting"
+    ><img src={scissors} alt="scissors" /></button
+  >
 
   {#if playerChoice}
-    <div
-      class="choice"
-      transition:fly={{ x: 200, y: -100, duration: 500 }}
-      style="top: 50%; left: 50%;"
-    >
-      {playerChoice}
+    <div class="choice" style="top: 50%; left: 50%;">
+      <img
+        src={rock}
+        alt="rock"
+        class="choice-rock"
+        class:selected-choice={playerChoice == "rock"}
+      />
+      <img
+        src={paper}
+        alt="paper"
+        class="choice-paper"
+        class:selected-choice={playerChoice == "paper"}
+      />
+      <img
+        src={scissors}
+        alt="scissors"
+        class="choice-scissors"
+        class:selected-choice={playerChoice == "scissors"}
+      />
     </div>
   {/if}
 </div>
@@ -29,12 +58,108 @@
     font-family: sans-serif;
   }
 
+  .btn-choice {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    border: 3px solid black;
+    background-color: rgb(159, 241, 255);
+  }
+
+  img {
+    pointer-events: none;
+    user-select: none;
+    -webkit-user-select: none;
+  }
+
+  .btn-rock.selecting {
+    top: 30%;
+    left: 40%;
+    transform: translate(-50%, -50%) rotate(-45deg);
+  }
+
+  .btn-paper.selecting {
+    top: 30%;
+    left: 60%;
+    transform: translate(-50%, -50%) rotate(45deg);
+  }
+
+  .btn-scissors.selecting {
+    top: 70%;
+    transform: translate(-50%, -50%);
+  }
+
+  .btn-choice img {
+    width: 100%;
+    height: 100%;
+  }
+
   .choice {
     position: absolute;
+    left: 50%;
+    top: 50%;
+    margin: -5px;
+    transform: translate(-50%, -50%);
+    width: 100px;
+    height: 100px;
+  }
+
+  .choice img {
+    display: none;
+    width: 100%;
+    height: 100%;
+    padding: 5px;
+    background-color: rgba(255, 255, 255, 0.4);
+    border-radius: 50%;
+  }
+
+  .choice .selected-choice {
+    display: block;
   }
 
   .game {
+    position: relative;
     width: 100vw;
     height: 100vh;
+    background-color: #fefefe;
+    background-image: repeating-linear-gradient(
+        45deg,
+        #ffb10a 25%,
+        transparent 25%,
+        transparent 75%,
+        #ffb10a 75%,
+        #ffb10a
+      ),
+      repeating-linear-gradient(
+        45deg,
+        #ffb10a 25%,
+        #ffd739 25%,
+        #ffd739 75%,
+        #ffb10a 75%,
+        #ffb10a
+      );
+    background-position:
+      0 0,
+      96px 96px;
+    background-size: 192px 192px;
+    overflow: hidden;
+    animation: moveBackground 10s linear infinite;
+  }
+
+  @keyframes moveBackground {
+    0% {
+      background-position:
+        0 0,
+        96px 96px;
+    }
+    100% {
+      background-position:
+        96px 96px,
+        192px 192px;
+    }
   }
 </style>
